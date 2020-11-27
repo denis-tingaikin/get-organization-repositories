1e32e16b70935630c7316ca6ae794f75f86fd162
Denis Tingajkin <denis.tingajkin@xored.com>
2020-11-27 20:23:43 +0700

    add regex pattern
    Signed-off-by: Denis Tingajkin <denis.tingajkin@xored.com>

--- README.md
+++ README.md
@@ -8,7 +8,13 @@ This returns list of repositories by passed organization.
 
 **Required** The GitHub organization.
 
+### `name-pattern`
+
+**Optional** The regex pattern to match repository names. By default `.*`.
+
 ## Outputs
+
+### `repositories`
 
 The list of repositories related to passed organization.
 
--- cmd/main.go
+++ cmd/main.go
@@ -3,26 +3,46 @@ package main
 import (
 	"context"
 	"os"
+	"regexp"
 	"strings"
 
 	"github.com/google/go-github/v32/github"
 )
 
+/*
+	os.Args[1] - regex pattern
+	os.Args[2] - org name
+*/
 func main() {
 	client := github.NewClient(nil)
 	opt := &github.RepositoryListByOrgOptions{Type: "public"}
-	repos, _, err := client.Repositories.ListByOrg(context.Background(), os.Args[1], opt)
+	if len(os.Args) != 3 {
+		panic("args len is not 3")
+	}
+	regex := regexp.MustCompile(os.Args[1])
+	orgName := os.Args[2]
+	repos, _, err := client.Repositories.ListByOrg(context.Background(), orgName, opt)
 	if err != nil {
 		panic(err.Error())
 	}
+	var result []*github.Repository
+	for _, r := range repos {
+		if !regex.MatchString(r.GetName()) {
+			continue
+		}
+		result = append(result, r)
+	}
+
 	var sb strings.Builder
+
 	_, _ = sb.WriteString("[")
-	for i, r := range repos {
+	for i, r := range result {
 		_, _ = sb.WriteString(r.GetFullName())
-		if i+1 < len(repos) {
+		if i+1 < len(result) {
 			_, _ = sb.WriteString(", ")
 		}
 	}
 	_, _ = sb.WriteString("]")
+
 	println("::set-output name=repositories::" + sb.String())
 }
